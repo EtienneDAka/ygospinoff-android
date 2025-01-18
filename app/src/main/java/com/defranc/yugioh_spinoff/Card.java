7b115ed90481a584769db85074789fbf7357207a
jorgedc <jddelcampom@gmail.com>
2025-01-18 11:57:19 -0500

    add: monster card, spell card and trap card

--- app/src/main/java/com/defranc/yugioh_spinoff/Card.java
+++ app/src/main/java/com/defranc/yugioh_spinoff/Card.java
@@ -1,40 +1,30 @@
 package com.defranc.yugioh_spinoff;
 
 public class Card {
-
-    public enum CardType {
-        MONSTER,
-        TRAP,
-        MAGIC
-    }
-
     public enum Orientation {
         ATTACK,
         DEFENSE
     }
 
     private String name;
-    private CardType cardType;
     private String description;
     private Orientation orientation;
     private int imageResId;
 
-    public Card(String name, CardType cardType, int imageResId, String description) {
+    public Card(
+            String name,
+            int imageResId,
+            String description
+    ) {
         this.name = name;
-        this.cardType = cardType;
         this.imageResId = imageResId;
         this.description = description;
-        this.orientation = Orientation.ATTACK; // Default
+        this.orientation = Orientation.ATTACK;
     }
 
     public String getName() { return name; }
-    public CardType getCardType() { return cardType; }
     public Orientation getOrientation() { return orientation; }
     public int getImageResId() { return imageResId; }
-
-    public boolean isMonsterCard() {
-        return cardType == CardType.MONSTER;
-    }
 
     public boolean isDefense() {
         return orientation == Orientation.DEFENSE;
@@ -43,23 +33,109 @@ public class Card {
     public void setDefense(boolean defense) {
         orientation = defense ? Orientation.DEFENSE : Orientation.ATTACK;
     }
-
-    public void setOrientation(Orientation orientation) {
-        this.orientation = orientation;
-    }
 }
 
 class MonsterCard extends Card {
     private int attack;
     private int defense;
-    private MonsterType monster_type;
+    private MonsterType type;
     private MonsterAttribute attribute;
 
-    public MonsterCard(String name, CardType cardType, int imageResId, String description, int attack, int defense, MonsterType monster_type, MonsterAttribute attribute) {
-        super(name, cardType, imageResId, description);
+    public MonsterCard (
+            String name,
+            int imageResId,
+            String description,
+            int attack,
+            int defense,
+            MonsterType type,
+            MonsterAttribute attribute
+    ) {
+        super(name, imageResId, description);
         this.attack = attack;
         this.defense = defense;
-        this.monster_type = monster_type;
+        this.type = type;
         this.attribute = attribute;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public MonsterType getType() {
+        return type;
+    }
+
+    public MonsterAttribute getAttribute() {
+        return attribute;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+}
+
+class SpellCard extends Card {
+    private MonsterType affectedMonster;
+    private int attackBoost;
+    private int defenseBoost;
+    private boolean isActive;
+
+    public SpellCard (
+            String name,
+            int imageResId,
+            String description,
+            MonsterType affectedMonster,
+            int attackBoost,
+            int defenseBoost
+    ) {
+        super(name, imageResId, description);
+        this.affectedMonster = affectedMonster;
+        this.attackBoost = attackBoost;
+        this.defenseBoost = defenseBoost;
+        this.isActive = false;
+    }
+
+    public boolean activate(MonsterCard monster) {
+        if (monster.getType().getValue().equals(affectedMonster.getValue())) {
+            monster.setAttack(monster.getAttack() + attackBoost);
+            monster.setDefense(monster.getDefense() + defenseBoost);
+            return true;
+        }
+        return false;
+    }
+}
+
+class TrapCard extends Card {
+    private boolean isActive;
+    private MonsterAttribute affectsMonster;
+
+    public TrapCard (
+            String name,
+            int imageResId,
+            String description,
+            MonsterAttribute affectsMonster
+    ) {
+        super(name, imageResId, description);
+        this.isActive = false;
+        this.affectsMonster = affectsMonster;
+    }
+
+    public boolean activate(MonsterCard attackingMonster) {
+        if (attackingMonster.getAttribute().getValue().equals(affectsMonster.getValue())) {
+            isActive = true;
+            System.out.println(
+                    "La carta trampa '" + super.getName() + "' es activada y niega el ataque de '"
+                    + attackingMonster.getName() + "'!");
+            return true;
+        }
+        return false;
     }
 }
