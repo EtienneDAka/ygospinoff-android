232faa0f3597ddfbaddb67aa46359905e8f243e4
Etienne Defranc <fdefranc@espol.edu.ec>
2025-01-18 11:50:38 -0500

    commit 1

--- app/src/main/java/com/defranc/yugioh_spinoff/Card.java
+++ app/src/main/java/com/defranc/yugioh_spinoff/Card.java
@@ -52,8 +52,14 @@ public class Card {
 class MonsterCard extends Card {
     private int attack;
     private int defense;
-    private MonsterType type;
+    private MonsterType monster_type;
     private MonsterAttribute attribute;
 
-    public MonsterCard(String name, )
+    public MonsterCard(String name, CardType cardType, int imageResId, String description, int attack, int defense, MonsterType monster_type, MonsterAttribute attribute) {
+        super(name, cardType, imageResId, description);
+        this.attack = attack;
+        this.defense = defense;
+        this.monster_type = monster_type;
+        this.attribute = attribute;
+    }
 }
